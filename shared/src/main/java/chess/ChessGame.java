637a0bb35568af39eaf49f7f8362567e87d0009b
samirr2502 <samirrodriguez2502@gmail.com>
2024-10-03 00:57:15 -0600

    Implemented isInStalemate method. Just reused kingCantMove methode

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -232,10 +232,7 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        if (board!=null) {
-            return true;
-        }
-        return false;
+        return kingCantMove(teamColor,board);
     }
 
     /**
