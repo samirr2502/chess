c5b671bb7a66fecc38b34885a4d7659b75b767af
samirr2502 <samirrodriguez2502@gmail.com>
2024-10-26 12:55:27 -0600

    Added SQL Game class

--- server/src/main/java/dataaccess/gamedao/SQLGameDAO.java
+++ server/src/main/java/dataaccess/gamedao/SQLGameDAO.java
@@ -1,8 +1,10 @@
 package dataaccess.gamedao;
 
+import com.google.gson.Gson;
 import dataaccess.DataAccessException;
 import dataaccess.DatabaseManager;
 import model.GameData;
+import model.UserData;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -10,32 +12,94 @@ import java.util.ArrayList;
 public class SQLGameDAO implements GameDAO{
   @Override
   public GameData getGame(String gameName) throws DataAccessException, SQLException {
-    return null;
+    try (var conn = DatabaseManager.getConnection()) {
+      var statement = "SELECT json FROM gameData WHERE username=?";
+      var ps = conn.prepareStatement(statement);
+      ps.setString(1, gameName);
+      try (var rs = ps.executeQuery()) {
+        if (rs.next()) {
+          String json = rs.getString("json");
+          return new Gson().fromJson(json, GameData.class);
+        }
+        return null;
+      }
+    } catch (Exception e) {
+      throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+    }
   }
 
   @Override
   public GameData getGameByID(int gameID) throws DataAccessException, SQLException {
-    return null;
+    try (var conn = DatabaseManager.getConnection()) {
+      var statement = "SELECT json FROM gameData WHERE id=?";
+      var ps = conn.prepareStatement(statement);
+      ps.setInt(1, gameID);
+      try (var rs = ps.executeQuery()) {
+        if (rs.next()) {
+          String json = rs.getString("json");
+          return new Gson().fromJson(json, GameData.class);
+        }
+        return null;
+      }
+    } catch (Exception e) {
+      throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+    }
   }
 
   @Override
   public ArrayList<GameData> getGames() throws DataAccessException, SQLException {
-    return null;
+    try (var conn = DatabaseManager.getConnection()) {
+      var statement = "SELECT json FROM gameData";
+      var ps = conn.prepareStatement(statement);
+      try (var rs = ps.executeQuery()) {
+        ArrayList<GameData> games = new ArrayList<>();
+        while (rs.next()) {
+          String json = rs.getString("json");
+          games.add(new Gson().fromJson(json, GameData.class));
+        }
+        return games;
+      }
+    } catch (Exception e) {
+      throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+    }
   }
 
   @Override
   public void addGameData(GameData gameData) throws DataAccessException, SQLException {
-
+    try (var conn = DatabaseManager.getConnection()) {
+      var statement = "INSERT INTO gameData (whiteUsername, blackUsername, gameName,game,json) values (?,?,?,?,?)";
+      var json = new Gson().toJson(gameData);
+      var game = new Gson().toJson(gameData.game());
+      try (var ps = conn.prepareStatement(statement)) {
+        ps.setString(1,gameData.whiteUsername());
+        ps.setString(2,gameData.blackUsername());
+        ps.setString(3,gameData.gameName());
+        ps.setString(4,game);
+        ps.setString(5,json);
+        ps.executeUpdate();
+      }
+    } catch (Exception e) {
+      throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+    }
   }
 
   @Override
   public void updateGame(GameData gameData) throws DataAccessException, SQLException {
-
+    deleteGameData(gameData);
+    addGameData(gameData);
   }
 
   @Override
   public void deleteGameData(GameData gameData) throws DataAccessException, SQLException {
-
+    try (var conn = DatabaseManager.getConnection()) {
+      var statement = "DELETE FROM gameData WHERE id=?";
+      try (var ps = conn.prepareStatement(statement)) {
+        ps.setInt(1,gameData.gameID());
+        ps.executeUpdate();
+      }
+    } catch (Exception e) {
+      throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+    }
   }
 
   @Override
---
+++ server/src/test/java/dataaccess/gamedao/SQLGameDAOTest.java
@@ -0,0 +1,46 @@
+package dataaccess.gamedao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SQLGameDAOTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void getGame() {
+  }
+
+  @Test
+  void getGameByID() {
+  }
+
+  @Test
+  void getGames() {
+  }
+
+  @Test
+  void addGameData() {
+  }
+
+  @Test
+  void updateGame() {
+  }
+
+  @Test
+  void deleteGameData() {
+  }
+
+  @Test
+  void deleteAllGames() {
+  }
+}
