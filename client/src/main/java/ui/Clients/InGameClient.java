549eeaa0b8a6af94edada7df43d174c739d80d2d
samirr2502 <samirrodriguez2502@gmail.com>
2024-11-12 22:56:56 -0700

    leaveGame option

--- client/src/main/java/ui/Clients/InGameClient.java
+++ client/src/main/java/ui/Clients/InGameClient.java
@@ -1,7 +1,10 @@
 package ui.Clients;
 
+import ui.Repl;
 import ui.ServerFacade;
 import ui.State;
+
+import java.util.Arrays;
 
 public class InGameClient implements ChessClient{
   private final ServerFacade server;
@@ -10,12 +13,49 @@ public class InGameClient implements ChessClient{
 
   }
   @Override
-  public String eval(String prompt) {
-    return null;
+  public String eval(String input) {
+    try {
+      var tokens = input.toLowerCase().split(" ");
+      var cmd = (tokens.length > 0) ? tokens[0] : "help";
+      var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+      return switch (cmd) {
+        case "board" -> getBoard(params);
+        case "surrender" -> surrender(params);
+        case "leave" -> leaveGame(params);
+        case "quit" -> "quit";
+        default -> help();
+      };
+    } catch (Exception ex) {
+      return ex.getMessage();
+    }
   }
 
+  private String getBoard(String[] params) {
+    return "";
+  }
+  private String surrender(String[] params) {
+    return "";
+  }
+  private String leaveGame(String[] params) {
+    Repl.state=State.LOGGED_IN;
+    return "You left the Game out \n\n Type help for commands";
+  }
+  private String drawBoardWhite(String[] params){
+    return """
+              1 2
+            b     b
+            a     a
+              1 2
+            """;
+  }
   @Override
   public String help() {
-    return null;
+    return """
+            Commands:
+            - getBoard games
+            - surrender <gameName>
+            - leave <GameID> <WHITE|BLACK>
+            - quit
+            - help""";
   }
 }
