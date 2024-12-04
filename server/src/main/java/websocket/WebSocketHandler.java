7a5d6620d456a302f34a0446901ee68b59a950ea
samirr2502 <samirrodriguez2502@gmail.com>
2024-12-03 22:08:37 -0700

    Added websocket manager to server module

---
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -0,0 +1,67 @@
+package websocket;
+
+import chess.ChessMove;
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+
+
+import java.io.IOException;
+
+
+@WebSocket
+public class WebSocketHandler {
+
+    private final ConnectionManager connections = new ConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+
+        switch (command.getCommandType()) {
+            case CONNECT -> connect(command.getAuthToken(),session);
+            case MAKE_MOVE-> makeMove(command.getAuthToken());
+            case LEAVE -> leave(command.getAuthToken());
+            case RESIGN -> resign(command.getAuthToken());
+        }
+    }
+
+    private void connect(String visitorName, Session session) throws IOException {
+        connections.add(visitorName, session);
+        //var message = String.format("%s is in the shop", visitorName);
+        var message = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME);
+        connections.broadcast(visitorName, message);
+    }
+    private void makeMove(String commandMove) throws IOException {
+        connections.remove(commandMove);
+        //var message = String.format("%s left the shop", commandMove);
+        var message = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+        connections.broadcast(commandMove, message);
+    }
+
+    private void leave(String visitorName) throws IOException {
+        connections.remove(visitorName);
+//        var message = String.format("%s left the shop", visitorName);
+        var message = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+        connections.broadcast(visitorName, message);
+    }
+    private void resign(String visitorName) throws IOException {
+        connections.remove(visitorName);
+        //var message = String.format("%s left the shop", visitorName);
+        var message = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+        connections.broadcast(visitorName, message);
+    }
+
+    public void makeNoise(String petName, String sound) throws Exception {
+        try {
+            var message = String.format("%s says %s", petName, sound);
+            //var notification = new Notification(Notification.Type.NOISE, message);
+            //connections.broadcast("", notification);
+        } catch (Exception ex) {
+           // throw new ResponseException(500, ex.getMessage());
+        }
+    }
+}
